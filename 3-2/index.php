6465d74db4427641eeaadad03ea324d105e8c0b1
chimaruyama <117245748+chimaruyama@users.noreply.github.com>
2022-12-25 13:04:45 +0900

    Create index.php

---
+++ 3-2/index.php
@@ -0,0 +1,15 @@
+<?php
+$fruits = ["りんご" => "150","みかん" => "50","もも" => "500"];
+
+$number = ["2","3","6"];
+
+function getPrice($fruits, $number){
+    $value = $fruits * $number;
+    return $value;
+}
+foreach($fruits as $key => $value){
+    echo $key."は".getPrice($value,$number[$i])."円です。";
+    echo '<br>';
+    $i++;
+}
+?>
