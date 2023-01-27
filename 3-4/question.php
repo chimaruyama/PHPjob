f70ab2aff91cb5464443895cf7f0304c6582b209
chimaruyama <117245748+chimaruyama@users.noreply.github.com>
2023-01-27 11:24:28 +0900

    Update question.php

--- 3-4/question.php
+++ 3-4/question.php
@@ -9,37 +9,30 @@ $answer1 = $question1[0];
 $answer2 = $question2[3];
 $answer3 = $question3[1];
 ?>
-
-
-
 <p>お疲れ様です<?php echo $my_name; ?>さん</p>
 
-<h2>①ネットワークのポート番号は何番？</h2>
+<form action="answer.php" method="post">
+   
+  
 
-<form method="POST" action="answer.php">
-   <?php foreach($question1 as $value){ ?>
-   <input type="radio" name="question1" value="<?php echo $value; ?>" /> <?php echo $value; ?>
-   <?php } ?>
-   <input type="hidden" name="answer1" value="<?php echo $answer1 ?>">
-</form>
+   <h2>①ネットワークのポート番号は何番？</h2>
+       <?php foreach($question1 as $value){ ?>
+       <input type="radio" name="question1" value="<?php echo $value; ?>" /> <?php echo $value; ?>
+       <?php } ?>
+       <input type="hidden" name="answer1" value="<?php echo $answer1; ?>"> -->
 
-
-<h2>②Webページを作成するための言語は？</h2>
-
-<form method="POST" action="answer.php">
-   <?php foreach($question2 as $value){ ?>
-   <input type="radio" name="question2" value="<?php echo $value; ?>" /> <?php echo $value; ?>
-   <?php } ?>
-   <input type="hidden" name="answer2" value="<?php echo $answer2 ?>">
-</form>
+   <h2>②Webページを作成するための言語は？</h2>
+       <?php foreach($question2 as $value){ ?>
+       <input type="radio" name="question2" value="<?php echo $value; ?>" /> <?php echo $value; ?>
+       <?php } ?>
+       <input type="hidden" name="answer2" value="<?php echo $answer2; ?>">
 
 
-<h2>③MySQLで情報を取得するためのコマンドは？</h2>
-
-<form method="POST" action="answer.php">
-   <?php foreach($question3 as $value){ ?>
-   <input type="radio" name="question3" value="<?php echo $value; ?>" /> <?php echo $value; ?>
-   <?php } ?>
-   <input type="hidden" name="answer3" value="<?php echo $answer3 ?>"><br>
-   <input type="submit" value="回答する">
+   <h2>③MySQLで情報を取得するためのコマンドは？</h2>
+       <?php foreach($question3 as $value){ ?>
+       <input type="radio" name="question3" value="<?php echo $value; ?>" /> <?php echo $value; ?>
+       <?php } ?>
+       <input type="hidden" name="answer3" value="<?php echo $answer3; ?>">
+       <input type="hidden" name="my_name" value="<?php echo $name; ?>"><br>
+       <input type="submit" value="回答する">
 </form>
