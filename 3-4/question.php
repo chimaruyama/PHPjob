a91f7913c55960414afcc089ff8aa483d8ed4762
chimaruyama <117245748+chimaruyama@users.noreply.github.com>
2023-01-30 13:15:51 +0900

    Update question.php

--- 3-4/question.php
+++ 3-4/question.php
@@ -15,27 +15,29 @@ $answer2 = $question2[3];
 $answer3 = $question3[1];
 // var_dump($answer3);
 ?>
-<p>お疲れ様です<?php echo $my_name; ?>さん</p>
+<body>
+   <p>お疲れ様です<?php echo $my_name; ?>さん</p>
 
-<form action="answer.php" method="post">
-   <h2>①ネットワークのポート番号は何番？</h2>
+   <form action="answer.php" method="post">
+       <h2>①ネットワークのポート番号は何番？</h2>
        <?php foreach($question1 as $value){ ?>
        <input type="radio" name="question1" value="<?php echo $value; ?>" /> <?php echo $value; ?>
        <?php } ?>
        <input type="hidden" name="answer1" value="<?php echo $answer1; ?>"> 
 
-   <h2>②Webページを作成するための言語は？</h2>
+       <h2>②Webページを作成するための言語は？</h2>
        <?php foreach($question2 as $value){ ?>
        <input type="radio" name="question2" value="<?php echo $value; ?>" /> <?php echo $value; ?>
        <?php } ?>
        <input type="hidden" name="answer2" value="<?php echo $answer2; ?>">
 
 
-   <h2>③MySQLで情報を取得するためのコマンドは？</h2>
+       <h2>③MySQLで情報を取得するためのコマンドは？</h2>
        <?php foreach($question3 as $value){ ?>
        <input type="radio" name="question3" value="<?php echo $value; ?>" /> <?php echo $value; ?>
        <?php } ?>
        <input type="hidden" name="answer3" value="<?php echo $answer3; ?>">
        <input type="hidden" name="my_name" value="<?php echo $my_name; ?>"><br>
        <input type="submit" value="回答する" />
-</form>
+    </form>
+</body>
