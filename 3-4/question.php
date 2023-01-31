a855c37493d1f0ac3878b0c1455ea2e1c29690f2
chimaruyama <117245748+chimaruyama@users.noreply.github.com>
2023-01-31 16:35:05 +0900

    Update question.php

--- 3-4/question.php
+++ 3-4/question.php
@@ -25,7 +25,7 @@ $answer3 = $question3[1];
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-        <p class="item">お疲れ様です<?php echo $my_name; ?>さん</p> 
+        <p>お疲れ様です<?php echo $my_name; ?>さん</p> 
         <form action="answer.php" method="post">
             <h2 class="item">①ネットワークのポート番号は何番？</h2>
             <?php foreach($question1 as $value){ ?>
@@ -33,13 +33,13 @@ $answer3 = $question3[1];
               <?php } ?>
             <input type="hidden" name="answer1" value="<?php echo $answer1; ?>"> 
 
-            <h2 class="item">②Webページを作成するための言語は？</h2>
+            <h2>②Webページを作成するための言語は？</h2>
             <?php foreach($question2 as $value){ ?>
               <input type="radio" name="question2" value="<?php echo $value; ?>" /> <?php echo $value; ?>
               <?php } ?>
             <input type="hidden" name="answer2" value="<?php echo $answer2; ?>">
 
-            <h2 class="item">③MySQLで情報を取得するためのコマンドは？</h2>
+            <h2>③MySQLで情報を取得するためのコマンドは？</h2>
             <?php foreach($question3 as $value){ ?>
             <input type="radio" name="question3" value="<?php echo $value; ?>" /> <?php echo $value; ?>
             <?php } ?>
